4b275a7ac400a7e9259ab99d3c2cdaaeb95746e8
erssi <ere.auvinen@gmail.com>
2024-02-04 19:47:01 +0200

    puhtaalla nodella esimerkkitestit ja app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+function ynnays(a, b) {
+    return a + b;
+  }
+
+function terve() {
+    return 'Hello world!';
+}
+
+function failure(a, b) {
+    return a + b + 1
+}
+module.exports = {
+    ynnays,
+    terve,
+    failure
+};
