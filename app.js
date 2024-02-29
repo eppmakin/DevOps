d839ffa532126237797d98f4211addcbaf89b2e0
auvieree <auvieree@jyu.fi>
2024-02-29 16:51:48 +0200

    dockerfilet ja gitignore

--- app.js
+++ app.js
@@ -6,14 +6,7 @@ function terve() {
     return 'Hello world!';
 }
 
-function check_spread(price, bid) {
-    let difference = Math.abs(price - bid);
-    let tenPercent = Math.max(price, bid) * 0.1;
-    return difference <= tenPercent;
-}
-
 module.exports = {
     ynnays,
     terve,
-    check_spread
 };
