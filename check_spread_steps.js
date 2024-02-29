5dfe04d5934be5df0d5a493c1c157ad712eb4af8
auvieree <auvieree@jyu.fi>
2024-02-29 17:20:08 +0200

    feature, test, ja source-kansiot

---
+++ check_spread_steps.js
@@ -0,0 +1,19 @@
+const { Given, When, Then } = require('cucumber');
+const assert = require('assert');
+const app = require('./check_spread.js');
+
+Given('the price is {int}', function (price) {
+  this.price = price;
+});
+
+Given('the bid is {int}', function (bid) {
+  this.bid = bid;
+});
+
+When('I check the spread', function () {
+  this.result = app.check_spread(this.price, this.bid);
+});
+
+Then('the result should be {word}', function (expected) {
+  assert.strictEqual(this.result.toString(), expected);
+});
